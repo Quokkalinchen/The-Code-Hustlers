33fc33fa2c22383c4f59e95eee983707e8e7bed1
Jesus Jones <jonas.jaekel@gmail.com>
2017-02-18 16:07:54 +0100

    updated angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { LoginService } from './login.service';
+import { AuthService } from './auth.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    LoginService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
