c13b29d02bbe1f862b9ece5143840fece25e35d0
Jesus Jones <jonas.jaekel@gmail.com>
2017-02-22 01:04:48 +0100

    added changepassword service and implemented it

--- src/app/home/profile/changepassword/changepassword.component.ts
+++ src/app/home/profile/changepassword/changepassword.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { StudentService } from '../../../student.service';
+
 
 
 @Component({
@@ -8,22 +10,40 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./changepassword.component.css']
 })
 export class ChangepasswordComponent implements OnInit {
-  loginForm: FormGroup;
+  changePasswordForm: FormGroup;
   private response = "";
   private passwordOld = "";
   private passwordNew = "";
   private passwordRepeat = "";
 
-  constructor() { }
+  constructor(private studentService: StudentService) { }
 
   ngOnInit() {
-
+    this.changePasswordForm = new FormGroup({
+      passwordOld: new FormControl('', []),
+      passwordNew: new FormControl('', []),
+      passwordRepeat: new FormControl('', []),
+    });
 
   }
 
   changePassword(){
+   this.passwordOld = this.changePasswordForm.value.passwordOld;
+   this.passwordNew = this.changePasswordForm.value.passwordNew;
+   this.passwordRepeat = this.changePasswordForm.value.passwordRepeat;
 
-   alert("change password");
+
+   if(this.passwordNew == this.passwordRepeat){
+     this.studentService.changePasword(this.passwordOld,this.passwordNew).subscribe(
+      data => {
+        alert("Passwort wurde geändert");
+        this.response = data;
+      }
+     );;
+   }else{
+     alert("Die eingegebenen Kennwörter stimmen nicht überein");
+   }
+
   }
 
 }
--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -8,17 +8,56 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class StudentService {
-   headers: Headers;
 
   constructor(private http: Http) {
-    this.headers = new Headers();
-    this.headers.append('Authorization', localStorage.getItem('token'));
-    this.headers.append('Content-Type', 'application/json');
+
+
 }
 
   getStudentData() {
-    return this.http.get('http://46.101.204.215:1337/api/V1/student',this.headers).map(response => response.json());
+    var headers: Headers;
+    headers = new Headers();
+    headers.append('Authorization', localStorage.getItem('token'));
+    headers.append('Content-Type', 'application/json');
 
+    return this.http.get('http://46.101.204.215:1337/api/V1/student',{headers}).map(response => response.json());
+
+  }
+
+  changePasword(password, newpassword){
+    var headers: Headers;
+    headers = new Headers();
+    headers.append('Authorization', localStorage.getItem('token'));
+    headers.append('Content-Type', 'application/json');
+    return this.http.put('http://46.101.204.215:1337/api/V1/requestPasswordRecovery',
+        JSON.stringify({ password, newpassword })
+        , {headers}
+    ).map((res: Response) => {
+        if (res.status < 200 || res.status >= 300) {
+          console.log("error");
+          throw new Error('This request has failed ' + res.status);
+        }
+        else {
+          return res.json();
+        }
+    });
+  }
+
+  deleteProfile(password){
+    var headers: Headers;
+    headers = new Headers();
+    headers.append('Authorization', localStorage.getItem('token'));
+    headers.append('Content-Type', 'application/json');
+    return this.http.delete('http://46.101.204.215:1337/api/V1/student/', {headers}
+    ).map((res: Response) => {
+        if (res.status < 200 || res.status >= 300) {
+          console.log("error");
+          throw new Error('This request has failed ' + res.status);
+        }
+        else {
+          return res.json();
+        }
+    });
   }
 
 }
